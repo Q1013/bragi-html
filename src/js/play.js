b0ccc5fdab954e1e0c577165ecdece716b5c8ef9
Rodrigo Correia Braga <58756308+rodcorulfr@users.noreply.github.com>
2021-04-07 00:10:42 -0300

    Barra de progresso
    Barra de progresso e gif

--- src/js/play.js
+++ src/js/play.js
@@ -1,10 +1,24 @@
+var nameVideo = undefined,
+    value,
+    currentTime,
+    duration,
+    resultTime,
+    percent,
+    progress = document.getElementById('progress')
+
+window.onload = function () {
+    currentTime = player.getCurrentTime(), duration = player.getDuration()
+    setInterval(updateName, 1000)
+    setInterval(updateTime, 500)
+}
+
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player, name = undefined
+var player
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
@@ -17,30 +31,40 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-function onPlayerReady(event) {
-    event.target.playVideo()
-}
+// function onPlayerReady(event) {
+//     event.target.playVideo()
+// }
 
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-}
+// var done = false;
+// function onPlayerStateChange(event) {
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         setTimeout(stopVideo, 6000);
+//         done = true;
+//     }
+// }
+// function stopVideo() {
+//     player.stopVideo();
+// }
 
-setInterval(function () {
-    if (name != player.getVideoData().title) {
-        name = player.getVideoData().title
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+// }
+
+function updateName() {
+    if (nameVideo != player.getVideoData().title) {
+        nameVideo = player.getVideoData().title
         document.getElementById('name-music').innerText = player.getVideoData().title
     }
-}, 1000)
+}
 
-function onPlayerReady(event) {
-    event.target.playVideo();
+function updateTime() {
+    if (player.getPlayerState() == 1) {
+        currentTime = player.getCurrentTime()
+        percent = duration/100
+        resultTime = currentTime/percent
+        progress.style.width = resultTime+'%'
+    }
+
 }
 
 document.getElementById("previous").addEventListener("click", function () {
@@ -51,8 +75,8 @@ document.getElementById("play").addEventListener("click", function () {
     player.playVideo()
     document.getElementById('pause').style.display = "flex"
     document.getElementById('play').style.display = "none"
-
 });
+
 document.getElementById("pause").addEventListener("click", function () {
     player.pauseVideo()
     document.getElementById('pause').style.display = "none"
@@ -62,3 +86,4 @@ document.getElementById("pause").addEventListener("click", function () {
 document.getElementById("next").addEventListener("click", function () {
     player.nextVideo()
 });
+
